c80a984a3f972b8fd81f4bae199e9258c2c7bba8
Saqib Islam <saaqi.grw@gmail.com>
2024-07-11 11:29:14 +0500

    some minor navigation tweaks

--- src/js/_navigation.js
+++ src/js/_navigation.js
@@ -1,58 +1,61 @@
 /* ## Set Mobile Navigation stuff
 --------------------------------------------- */
 // selecting the element
-const menuLinks = document.querySelectorAll('.primary-menu .menu-item a, .go-top a, .site-title a, .shop-link');
-const menuButton = document.querySelector('.menu-toggle');
-const navigation = document.querySelector('nav.nav-primary');
-const toggleIcon = document.querySelector('.toggle-icon');
+const navLinks = document.querySelectorAll('.primary-menu .menu-item a, .go-top a, .site-title a, .shop-link')
+const menuButton = document.querySelector('.menu-toggle')
+const navigation = document.querySelector('nav.nav-primary')
+const toggleIcon = document.querySelector('.toggle-icon')
 
 
 // Functionality for main menu-toggle button
 menuButton.addEventListener('click', function () {
 
   // Show site navigation
-  navigation.classList.toggle('show');
+  navigation.classList.toggle('show')
 
   // toggle activated class
-  menuButton.classList.toggle('activated');
+  menuButton.classList.toggle('activated')
 
   // toggle attrs
   if (menuButton.getAttribute('aria-expanded') === 'true') {
-    toggleIcon.classList.remove('bx-x');
+    toggleIcon.classList.remove('bx-x')
   } else {
-    toggleIcon.classList.add('bx-x');
+    toggleIcon.classList.add('bx-x')
   }
 
   if (menuButton.getAttribute('aria-expanded') === 'true') {
-    menuButton.setAttribute('aria-expanded', 'false');
+    menuButton.setAttribute('aria-expanded', 'false')
   } else {
-    menuButton.setAttribute('aria-expanded', 'true');
+    menuButton.setAttribute('aria-expanded', 'true')
   }
 
   if (menuButton.getAttribute('aria-pressed') === 'true') {
-    menuButton.setAttribute('aria-pressed', 'false');
+    menuButton.setAttribute('aria-pressed', 'false')
   } else {
-    menuButton.setAttribute('aria-pressed', 'true');
+    menuButton.setAttribute('aria-pressed', 'true')
   }
 
 });
 
 // Functionality of individual links
-for (var eachLink of menuLinks) {
+for (var eachLink of navLinks) {
   eachLink.addEventListener('click', function () {
 
     // Hide Main Navigation on click
-    navigation.classList.remove('show');
+    navigation.classList.remove('show')
 
     // remove activated class and attrs from menu-toggle button
-    menuButton.classList.remove('activated');
-    menuButton.setAttribute('aria-expanded', 'false');
-    menuButton.setAttribute('aria-pressed', 'false');
-    toggleIcon.classList.remove('bx-x');
+    menuButton.classList.remove('activated')
+    menuButton.setAttribute('aria-expanded', 'false')
+    menuButton.setAttribute('aria-pressed', 'false')
+    toggleIcon.classList.remove('bx-x')
 
   });
 }
 
+
+navigation.setAttribute('tabindex', '-1')
+navigation.setAttribute('aria-hidden', 'true')
 
 
 
