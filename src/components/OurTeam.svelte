1462bcdcbca8254846c458cbfcd77ff36eb98ef0
Saqib Islam <saaqi.grw@gmail.com>
2025-06-22 00:11:37 +0500

    minor adjustments.

--- src/components/OurTeam.svelte
+++ src/components/OurTeam.svelte
@@ -1,20 +1,24 @@
 <script>
-	import basharatButt from '$assets/team/basharat_butt.webp?enhanced&w=300&format=avif;webp';
-	import javedButt from '$assets/team/javed_butt.webp?enhanced&w=300&format=avif;webp';
-	import tariqButt from '$assets/team/tariq_butt.webp?enhanced&w=300&format=avif;webp';
-	import saleemButt from '$assets/team/saleem_butt.webp?enhanced&w=300&format=avif;webp';
+	const profilePics = import.meta.glob('$assets/team/*.webp', {
+		eager: true,
+		query: {
+			enhanced: true,
+			w: '300',
+			format: 'avif;webp'
+		}
+	});
 
 	const teamMembers = [
 		{
 			name: 'Basharat Ali Butt',
 			position: 'Managing Director',
-			image: basharatButt,
+			image: 'basharat_butt.webp',
 			alt: 'Mr. Basharat Ali'
 		},
 		{
 			name: 'Javed Butt',
 			position: 'Chief Executive Officer',
-			image: javedButt,
+			image: 'javed_butt.webp',
 			alt: 'Mr. Javed Butt',
 			contact: [
 				{ icon: 'bxs-mobile', link: 'tel:+923008644389', label: '0300-8644389' },
@@ -24,13 +28,13 @@
 		{
 			name: 'Tariq Butt',
 			position: 'Chief Operating Officer',
-			image: tariqButt,
+			image: 'tariq_butt.webp',
 			alt: 'Mr. Tariq Butt'
 		},
 		{
 			name: 'Saleem Butt',
 			position: 'Head of Finance',
-			image: saleemButt,
+			image: 'saleem_butt.webp',
 			alt: 'Mr. Saleem Butt',
 			contact: [
 				{ icon: 'bxs-envelope', link: 'mailto:ikramsteelindustries@gmail.com', label: 'E-Mail' },
@@ -49,19 +53,23 @@
 			{#each teamMembers as { name, position, image, alt, contact }, index (('team-member-', index))}
 				<div class="team-widget col-6 col-lg-3">
 					<div class="card card-warning bg-primary-subtle text-dark h-100">
-						<enhanced:img
-						sizes="(min-width: 300px) 300px, 100vw"
-						class="img-fluid w-100 card-img-top"
-						src={image}
-						loading="lazy"
-						{alt}
-						/>
+						{#each Object.entries(profilePics) as [_path, module], index ('pic-' + index)}
+							{#if _path.includes(image)}
+								<enhanced:img
+									sizes="(min-width: 300px) 300px, 100vw"
+									class="img-fluid w-100 card-img-top"
+									src={module.default}
+									loading="lazy"
+									{alt}
+								/>
+							{/if}
+						{/each}
 						<div class="card-body">
 							<h4 class="names">{name}</h4>
 							<div>{position}</div>
 							{#if contact}
 								<ul class="team-contact list-unstyled mt-3">
-									{#each contact as { icon, link, label }, index('contact-', index)}
+									{#each contact as { icon, link, label }, index (('contact-', index))}
 										<li class="mb-3">
 											<a href={link}><i class="bx {icon}"></i> {label}</a>
 										</li>
