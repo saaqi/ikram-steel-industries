b65b8deca12ccd143305e507584073cc9e92a7f1
Saqib Islam <saaqi.grw@gmail.com>
2025-06-21 22:14:39 +0500

    minor bug fixes

--- src/components/OurTeam.svelte
+++ src/components/OurTeam.svelte
@@ -49,21 +49,19 @@
 			{#each teamMembers as { name, position, image, alt, contact }, index (('team-member-', index))}
 				<div class="team-widget col-6 col-lg-3">
 					<div class="card card-warning bg-primary-subtle text-dark h-100">
-						<div class="card-header">
-							<h4 class="h6 mb-0 text-center h5">{position}</h4>
-						</div>
 						<enhanced:img
-							sizes="(min-width: 300px) 300px, 100vw"
-							class="img-fluid w-100"
-							src={image}
-							loading="lazy"
-							{alt}
+						sizes="(min-width: 300px) 300px, 100vw"
+						class="img-fluid w-100 card-img-top"
+						src={image}
+						loading="lazy"
+						{alt}
 						/>
-						<div class="card-body d-flex flex-column align-items-center align-items-sm-start">
-							<h5 class="names">{name}</h5>
+						<div class="card-body">
+							<h4 class="names">{name}</h4>
+							<div>{position}</div>
 							{#if contact}
 								<ul class="team-contact list-unstyled mt-3">
-									{#each contact as { icon, link, label }, contactIndex (('contact-', contactIndex))}
+									{#each contact as { icon, link, label }, index('contact-', index)}
 										<li class="mb-3">
 											<a href={link}><i class="bx {icon}"></i> {label}</a>
 										</li>
